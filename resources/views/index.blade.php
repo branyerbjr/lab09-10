9749bd50233421a93349a61c064d4c0fe725b680
Branyer Romero <108288688+branyerbjr@users.noreply.github.com>
2023-11-24 21:06:35 -0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAB 11</title>
+    <title>LAB 9 - 10</title>
 </head>
 <body>
     <div>
